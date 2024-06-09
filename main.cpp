51f67bb916e1fcdeb1506cfa21eadcb657da3034
13petabytes <150091598+13petabytes@users.noreply.github.com>
2024-06-09 13:07:41 -0600

    main.cpp

--- main.cpp
+++ main.cpp
@@ -3,6 +3,9 @@
 #include <iostream>
 #include <stdio.h>
 #include <string>
+#include <vector>
+#include <memory>
+#include "Item.h"
 #include "DVD.h"
 #include "Libro.h"
 #include "Figuras.h" 
@@ -10,6 +13,8 @@
 using namespace std;
 
 int main() {
+  //Declaración de objetos y vector
+  vector<unique_ptr<Item>> items;
   Registrador archivador;
   cout<<"Bienvenido al sistema de archivos"<<endl;
   cout<<"Incerte la opción que desea realizar:"<<endl;
@@ -27,6 +32,49 @@ int main() {
   }
   //While para checar que el usuario no quiere seguir en el sistema
   while (opcion != 3){
+  //ciclo para crear un nuevo item
+  while (opcion == 1){
+    int nDVDs;
+    int nLibros;
+    int nFiguras;
+    int tItems;
+    cout<<"Incerte el numero de DVDs que va a archivar:"<<endl;
+    cin>>nDVDs;
+    cout<<"Incerte el numero de Libros que va a archivar:"<<endl;
+    cin>>nLibros;
+    cout<<"Incerte el numero de Figuras que va a archivar:"<<endl;
+    cin>>nFiguras;
+      for (int i = 0; i < nDVDs; ++i){
+        items.push_back(make_unique<DVD>());
+      }
+      for (int i = 0; i < nLibros; ++i){
+        items.push_back(make_unique<Libro>());
+      }
+      for (int i = 0; i < nFiguras; ++i){
+        items.push_back(make_unique<Figuras>());
+      }
+    tItems = nDVDs + nLibros + nFiguras;
+    for (int i = 0; i < tItems; ++i){
+      items[i]->crearItem();
+      items[i]->unirTXT();
+      string texto = items[i]->getTexto();
+      archivador.setTxt(texto);
+      archivador.archivar();
+    }
+    cout<<"¿Qué desea hacer ahora?"<<endl;
+    cout<<"1) para crear un nuevo item"<<endl;
+    cout<<"2) para buscar un item"<<endl;
+    cout<<"3) para salir"<<endl;
+    cin>>opcion;
+    while(opcion != 1 && opcion != 2 && opcion != 3){
+      cout<<"Incerte una opción valida"<<endl;
+      cout<<"1) para crear un nuevo item"<<endl;
+      cout<<"2) para buscar un item"<<endl;
+      cout<<"3) para salir"<<endl;
+      cin>>opcion;
+    }
+    }
+  //ciclo para buscar item
   while(opcion == 2){
     string letra = "x";
     int numero = 0;
@@ -64,7 +112,8 @@ int main() {
       cin>>opcion;
     }
   }
-  return 0;
   }
-  cout<<"Adios";
+
+cout<<"Adios";
+return 0;
 }
