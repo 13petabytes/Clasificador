5ea5001c3746c38725bb25a8e8f81f4bef18311e
13petabytes <150091598+13petabytes@users.noreply.github.com>
2024-06-09 13:02:25 -0600

    DVD.h

--- DVD.h
+++ DVD.h
@@ -4,22 +4,23 @@
 #include <string>
 #include <iostream>
 #include <stdio.h>
+#include <memory>
 #include <string>
 #include "Item.h"
 using namespace std;
 
 class DVD:public Item{
-protected:
+private:
   string dvdTxt;
 public:
   //constructor y destructor
   DVD();
-  ~DVD();
+  ~DVD() override;
   //getter y setter
   string getDVDtxt();
   void setDVDtxt(string);
   //metodos
   void unirTXT() override;
-  void creadorDvdtxt();
+  void crearItem() override;
 };
 #endif //DVD_h
